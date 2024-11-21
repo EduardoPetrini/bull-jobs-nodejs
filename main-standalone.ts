082b1beceb8ce9e9e686cf9e7bbc2e7d3134f1c7
EduardoPetrini <eduardo.petrini@outlook.com>
2024-11-21 19:09:05 -0300

    chore: rename main

--- main-standalone.ts
+++ main-standalone.ts
@@ -5,10 +5,11 @@ import readline from 'readline';
 import Queue from 'bull';
 import { listen } from './events';
 import { startWorker } from './worker';
+import { logInfo } from './utils/utils';
 
 const start = async () => {
   const mode = process.argv[2] || 'main';
-  console.log('Starting...')
+  logInfo('Starting...')
   const HOST = process.env.REDIS_HOST || 'localhost';
   const PORT = parseInt(process.env.REDIS_PORT || '6379');
 
@@ -21,24 +22,12 @@ const start = async () => {
     return startWorker(myQueue);
   }
 
-  setTimeout(() => {
-    console.log('Adding a job')
-    myQueue.add({ title: 'hey ho' })
-  }, 3000)
+  setInterval(async () => {
+    logInfo('count', await myQueue.count());
+    logInfo('workers', (await myQueue.getWorkers()).length);
+  }, 1000)
 }
 
 start().then(() => {
-  console.log('Done')
-})
-
-readline.emitKeypressEvents(process.stdin);
-let keyPressed = [];
-
-process.stdin.on('keypress', (ch, key) => {
-  keyPressed.push(key.name);
-  keyPressed.splice(0, keyPressed.length - 2);
-  if (keyPressed[0] === 'q' && keyPressed[1] === 'return') {
-    console.log('Bye!');
-    process.exit(0)
-  }
+  logInfo('Done')
 })
